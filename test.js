e318ba0436c6c0683b9b90b7c692406e32277a28
Zhang que <112451217+copilotuser1@users.noreply.github.com>
2023-05-25 11:26:52 +0800

    111

--- test.js
+++
@@ -1,8 +0,0 @@
-const path = './test'
-const name = 'test-list'
-
-const { genorTable, genorAddUpdate } =require("./app")
-const {params, addUpdateParams,}  = require("./genorator-config")
-
-genorTable(path, name, params)
-genorAddUpdate(path, name, addUpdateParams)
--- test/test.js
+++ test/test.js
@@ -1,14 +1,49 @@
-const {replaceMatch,   writeFile, getTemplates } = require("../lib/genorator")
+const { replaceMatch, getTemplate, writeFile, getTemplates, replaceState } = require("../lib/genorator")
 
+let options = {
+ path: './test',
+ name: 'supplier.vue',
+ str: [{
+  template: 'pages/params',
+  templatePath: "", // 
+  params: {
+   filterList: [{
+    template: 'components/input',
+    params: {
+     label: 'aX',
+     prop: 'ab',
+    }
+   }, {
+    template: 'components/input',
+    params: {
+     label: 'aw',
+     prop: 'awafa',
+    }
+   },],
+   permissionPath: 'supplier:supplier',
+   tableColumns: [{
+    template: 'components/table-column',
+    params: {
+     type: '',
+     label: '',
+     prop: '',
+    }
+   }],
+   filename: 'supplier',
+   servicePath: 'supplier/supplier',
+   dataForm: JSON.stringify({
+    id: "",
+    type: "",
+    name: "",
+   })
+  }
 
-async function genPage1() {
- const templates = await getTemplates()
- const filterList = []
- const rdata = replaceMatch(templates.components.input, {
-  prop: 'prop',
-  label: '拉贝'
- })
- console.log(rdata)
+ }]
+}
+
+async function genPage1 () {
+ options.str = await replaceState(options.str)
+ writeFile(options.path, options.name, options.str)
 }
 
 genPage1()
